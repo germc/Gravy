e4ed6e9521b23dcc9a7a5f8e7c39129d9031afa5
Nathan Tesler <nathan@tesler.me>
2013-02-25 12:23:08 +1100

    Initial Commit

---
+++ Gravy/GRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRAppDelegate.h
+//  Gravy
+//
+//  Created by Nathan Tesler on 25/02/13.
+//  Copyright (c) 2013 Nathan Tesler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gravy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gravy
+//
+//  Created by Nathan Tesler on 25/02/13.
+//  Copyright (c) 2013 Nathan Tesler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRAppDelegate class]));
+    }
+}
